7ed43208cd837aabccf433090171fc69ba588a6a
mendrika.irina <mendrika.irina@gmail.com>
2024-08-21 12:37:56 +0000

    Gemini integration

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
-from flask import Flask
-import services.requesters.hello as hellow
+from flask import Flask, request
+import services.business.read_pdf_assist as hellow
 
 import os
 
@@ -8,9 +8,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello, '+hellow.getRoot() + '!' 
+    return 'Hello!'
 
+@app.route('/answer',  methods=['POST'])
+def answer_about():
 
+    request_data = request.get_json()
+    path = request_data['path']
+    prompt = request_data['prompt']
+
+    return hellow.access_pdf(path, prompt)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=os.environ.get('FLASK_PORT', 5000), debug=os.environ.get('FLASK_DEBUG', True))
---
+++ services/business/read_pdf_assist.py
@@ -0,0 +1,25 @@
+import services.requesters.google_cloud_storage as gs
+import os
+import vertexai
+from vertexai.generative_models import GenerativeModel, Part
+
+def access_pdf(path_in_bucket, prompt):
+
+    # TODO(developer): Update and un-comment below lines
+    project_id = os.getenv('PROJECT_NAME')
+
+    vertexai.init(project=project_id, location="europe-west1")
+
+    model = GenerativeModel("gemini-1.5-flash-001")
+
+    #data = gs.read_binary(os.getenv("BUCKET_NAME"), "groupe_bovis/CDC-TMS.pdf")
+    data = gs.read_binary(os.getenv("BUCKET_NAME"), path_in_bucket)
+
+    pdf_file = Part.from_data(data, mime_type="application/pdf")
+    contents = [pdf_file, prompt]
+    response = model.generate_content(contents)
+
+
+    #data = gs.gs_read_binary("gs://hypermanager_project_data/groupe_bovis/CDC-TMS.pdf")
+
+    return response.text
