9bc11cbd021dc87ec6a3a07c3af59ce818cafe61
mendrika.irina <mendrika.irina@gmail.com>
2024-11-18 11:58:29 +0000

    prepare for history

--- main.py
+++ main.py
@@ -23,8 +23,9 @@ def answer_about():
     request_data = request.get_json()
     path = request_data['path']
     prompt = request_data['prompt']
+    history = request_data['history']
 
-    return hellow.access_pdf(path, prompt)
+    return hellow.access_pdf(path, prompt, history)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=os.environ.get('FLASK_PORT', 5000), debug=os.environ.get('FLASK_DEBUG', True))
--- services/business/read_pdf_assist.py
+++ services/business/read_pdf_assist.py
@@ -3,7 +3,7 @@ import os
 import vertexai
 from vertexai.generative_models import GenerativeModel, Part
 
-def access_pdf(path_in_bucket, prompt):
+def access_pdf(path_in_bucket, prompt, history):
 
     # TODO(developer): Update and un-comment below lines
     project_id = os.getenv('PROJECT_NAME')
@@ -11,11 +11,19 @@ def access_pdf(path_in_bucket, prompt):
     vertexai.init(project=project_id, location="europe-west1")
 
     model = GenerativeModel("gemini-1.5-flash-001")
-
-    data = gs.read_binary(os.getenv("BUCKET_NAME"), path_in_bucket)
-    pdf_file = Part.from_data(data, mime_type="application/pdf")
-
-    contents = [pdf_file, prompt]
+    if path_in_bucket != "":
+        data = gs.read_binary(os.getenv("BUCKET_NAME"), path_in_bucket)
+        pdf_file = Part.from_data(data, mime_type="application/pdf")
+        if history != "":
+            contents = [pdf_file, prompt, history]
+        else:
+            contents = [pdf_file, prompt]
+    else:
+        if history != "":
+            contents = [prompt, history]
+        else:
+            contents = [prompt]
+    
 
     response = model.generate_content(contents)
 
