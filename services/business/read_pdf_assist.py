1e8d7a860214ee3ae457fa57d0cad14664f0200a
mendrika.irina <mendrika.irina@gmail.com>
2024-11-18 12:58:51 +0000

    introducing history

--- services/business/read_pdf_assist.py
+++ services/business/read_pdf_assist.py
@@ -1,6 +1,7 @@
 import services.requesters.google_cloud_storage as gs
 import os
 import vertexai
+import json
 from vertexai.generative_models import GenerativeModel, Part
 
 def access_pdf(path_in_bucket, prompt, history):
@@ -14,13 +15,13 @@ def access_pdf(path_in_bucket, prompt, history):
     if path_in_bucket != "":
         data = gs.read_binary(os.getenv("BUCKET_NAME"), path_in_bucket)
         pdf_file = Part.from_data(data, mime_type="application/pdf")
-        if history != "":
-            contents = [pdf_file, prompt, history]
+        if history :
+            contents = [pdf_file, json.dumps(history), prompt]
         else:
             contents = [pdf_file, prompt]
     else:
-        if history != "":
-            contents = [prompt, history]
+        if history :
+            contents = [json.dumps(history), prompt]
         else:
             contents = [prompt]
     
